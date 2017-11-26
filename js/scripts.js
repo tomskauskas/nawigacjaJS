27328865cfabdefd2c206949470dc461bd434433
tomski <tomskauskas@gmail.com>
2017-11-26 17:43:46 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+'use strict';
+var navi = document.getElementById('navi');
+navi.innerHTML = '<ul><a href="#"><li>start</li></a><a href="#"><li>firma</li></a><a href="#"><li>produkty</li></a><a href="#"><li>galeria</li></a><a href="#"><li>kontakt</li></a></ul>';
+
